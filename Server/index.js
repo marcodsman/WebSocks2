f6670394f9418282e05ddcdab30f6d4f40bff282
Marco <marcodsman@gmail.com>
2021-07-19 16:36:22 +0200

    bla

--- Server/index.js
+++ Server/index.js
@@ -1,7 +1,15 @@
 const WebSocket = require('ws');
+const express = require("express");
+const app = express();
+const path = require("path");
+
+
+app.use(express.static(path.resolve(__dirname, "../Client")))
+
+const server = app.listen(9876);
 
 const wss = new WebSocket.Server({
-	port: 9876,
+	port: 9876
 });
 
 
@@ -16,5 +24,18 @@ wss.on('connection', function(ws){
 	    });
 	});
 });
+
+
+// server.on('upgrade', async function upgrade(request, socket, head) {
+// 	// Do what you normally do in `verifyClient()` here and then use
+// 	// `WebSocketServer.prototype.handleUpgrade()`
+// 	// Socket param is raw socket not ws
+
+// 	return socket.end("HTTP/1.1 401 Unauthorized\r\n", "ascii");
+
+// 	wss.handleUpgrade(request, socket, head, function done(ws) {
+// 		wss.emit('connection', ws, request, ...args);
+// 	});
+// });
 
 console.log(wss);
